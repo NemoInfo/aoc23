f604c56a91cb23103e496196f22bce8bf479b57f
Aaron Panaitescu <aaronpanaitescu@gmail.com>
2023-12-13 07:15:31 +0000

    day 13 part 2 bit masks.

--- python/day-13/part2.py
+++ python/day-13/part2.py
@@ -1,25 +1,31 @@
-patterns = [*map(str.splitlines, open(0).read().split("\n\n"))]
+patterns =[
+    *map(str.splitlines, open(0).read().translate(str.maketrans(".#", "01")).split("\n\n"))
+]
 
-def check_horizontal(pattern):
-    for r in range(len(pattern) - 1):
+def check_horizontal(m):
+    for r in range(len(m) - 1):
         smudge_found = False
-        for i in range(min(r, len(pattern) - r - 2) + 1):
-            for c in range(len(pattern[0])):
-                if pattern[r - i][c] != pattern[r + 1 + i][c]:
-                    if smudge_found:
-                        break
-                    smudge_found = True
-            else:
+        for i in range(min(r, len(m) - r - 2) + 1):
+            diff = m[r - i] ^ m[r + 1 + i]
+            if not diff:
+                continue
+            if not diff & (diff - 1):
+                if smudge_found:
+                    break
+                smudge_found = True
                 continue
             break
         else:
             if smudge_found:
                 return r
 
+def to_ints(m):
+    return [*map(lambda x: int(x, base=2), m)]
+
 
 total = 0
 for pattern in patterns:
-    horizontal = check_horizontal(pattern)
+    horizontal = check_horizontal(to_ints(pattern))
 
     if horizontal is not None:
         total += 100 * (horizontal + 1)
@@ -30,6 +36,6 @@ for pattern in patterns:
         for c in range(len(pattern[0]))
     ]
 
-    total += 1 + check_horizontal(pattern_t)
+    total += 1 + check_horizontal(to_ints(pattern_t))
 
 print(total)
